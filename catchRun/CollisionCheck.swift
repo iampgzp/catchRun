4d4b7204c1f402d008e30a223e935de7b089c3c2
adiggo <adiggo@gmail.com>
2015-01-08 21:34:03 -0800

    add trap and wall check

--- catchRun/CollisionCheck.swift
+++ catchRun/CollisionCheck.swift
@@ -22,50 +22,65 @@ class CollisionCheck : NSObject{
     
     
     //check whether player has collision with walls
-    func checkIsCollision(player: PlayerNode) -> Bool{
-        if !checkIsOutOfBound(player){
-            return false
+    func checkIsCollision(player: PlayerNode, tileMap: JSTileMap) -> Bool{
+        var wall = tileMap.layerNamed("Meta")
+        var tileGid = wall.tileGidAt(CGPoint(player.position.x, player.position.y))
+        var properties:NSDictionary = tileMap.propertiesForGid(tileGid) as NSDictionary
+        var collision: NSString = properties.valueForKey("Collidable") as NSString
+        if collision.isEqualToString("True"){
+            // collide should prevent user go in to the wall
+            return true
         }
-        return true
+        return false
+    }
+    
+    func checkIsTrap(player: PlayerNode, tileMap: JSTileMap) -> Bool{
+        var trap = tileMap.layerNamed("trap")
+        var tileGid = trap.tileGidAt(CGPoint(player.position.x, player.position.y))
+        var properties:NSDictionary = tileMap.propertiesForGid(tileGid) as NSDictionary
+        var collision: NSString = properties.valueForKey("Collidable") as NSString
+        if collision.isEqualToString("True"){
+            // collide should prevent user go in to the wall
+            return true
+        }
+        return false
     }
     
     func checkIsOutOfBound(player: PlayerNode) -> Bool{
-        //if it is out of bound
+        //if it is out of bound, assume it is starting is at (0, 0), need to define a variable for its starting point
         if player.position.x < 0 {
-            return false
+            return true
         }
         if player.position.x > self.mapXSize{
-            return false
+            return true
         }
         if player.position.y < 0{
-            return false
+            return true
         }
         if player.position.y > self.mapYSize{
-            return false
+            return true
         }
-        return true
+        return false
     }
     
-    
-    func checkIsWall(player: PlayerNode) -> Bool{
-        if isOccupiedByWall(player.position){
-            return false
-        }
-        return true
-    }
+//    since the end
+//    func checkIsWall(player: PlayerNode) -> Bool{
+//        if isOccupiedByWall(player.position){
+//            return false
+//        }
+//        return true
+//    }
     
     // input the current location, judge whether it is a wall
     // need to fill later
-    func isOccupiedByWall(loc: CGPoint) -> Bool{
-        return true
-    }
+
     
     // if the location is occupied by other player
     func checkIsOtherPlayerInTheLoc(player1:PlayerNode, player2:PlayerNode) -> Bool{
         if player1.position != player2.position{
-            return true
-        }else{
             return false
+        }else{
+            return true
         }
     }
     
